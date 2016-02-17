b5c8de40446bfef6d673ab7fc620d6c29349b6aa
wuhaojie <w19961009@126.com>
2016-02-17 22:56:19 +0800

    fixed.

--- app/src/main/java/top/wuhaojie/multiswitchbutton/MainActivity.java
+++ app/src/main/java/top/wuhaojie/multiswitchbutton/MainActivity.java
@@ -1,6 +1,7 @@
 package top.wuhaojie.multiswitchbutton;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.SeekBar;
 
@@ -13,13 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         sb_main = (SeekBar) findViewById(R.id.sb_main);
         sb_main.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
+                Snackbar.make(findViewById(R.id.cl_main), "多级开关拨动到: " + progress, Snackbar.LENGTH_SHORT).show();
             }
+
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
@@ -28,18 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-//                int seekProgress = seekBar.getProgress();
-//                if(seekProgress<13){
-//                    seekBar.setProgress(0);
-//                }else if(seekProgress>=13 && seekProgress<38){
-//                    seekBar.setProgress(25);
-//                }else if(seekProgress>=38 && seekProgress<63){
-//                    seekBar.setProgress(50);
-//                }else if(seekProgress>=63 && seekProgress<88){
-//                    seekBar.setProgress(75);
-//                }else if(seekProgress>=88){
-//                    seekBar.setProgress(100);
-//                }
+
             }
         });
     }
